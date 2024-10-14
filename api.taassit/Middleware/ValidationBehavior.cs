a61765e54a43065becc37d3abefc7f82f6d290c6
mcuomo <michael.r.cuomo@gmail.com>
2024-10-14 19:03:43 -0400

    Initial API Setup

---
+++ api.taassit/Middleware/ValidationBehavior.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Us.Api.Ochlocracy.Middleware
+{
+    /// <summary>
+    /// A pipeline used to handle verification.
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> validators;
+
+        /// <summary>
+        /// The constructor for <see cref="ValidationBehavior{Request, Response}"/>.
+        /// </summary>
+        /// <param name="validators"></param>
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => this.validators = validators;
+
+        /// <summary>
+        /// The handler for the pipeline.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        /// <exception cref="ValidationException"></exception>
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var failures = validators
+                .Select(x => x.Validate(context))
+                .SelectMany(x => x.Errors)
+                .Where(x => x != null)
+                .ToList();
+
+            if (failures.Any())
+                throw new ValidationException(failures);
+
+            return next();
+        }
+    }
+}
